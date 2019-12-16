7f8204be462de6edc8d3ad04b9c8eb378f09e222
jmhammock <jason@arrayschool.com>
2019-12-16 14:01:23 -0700

    images

---
+++ app/app.js
@@ -0,0 +1,11 @@
+/**
+ * ONE: create a new branch with the name develop-{your name}
+ * 
+ * TWO: write some JavaScript to find the article element with an id that has your name.
+ * Using that element as a starting point, select the element with class="title" and change the innerHtml to be 
+ * your full name. Next, Select the element with a class="subtitle" and add a class of "twitter-handle" to the classList.
+ * Finally, select the img element and setAttribute src to the relative url string that loads your picture.
+ * 
+ * THREE: write some JavaScript that finds ALL articles with a class="professor". Loop through each element and set the child
+ * img element src to the relative clown file. Change the title to 'Clown', and subtitle to '@arrayClown' 
+ */
